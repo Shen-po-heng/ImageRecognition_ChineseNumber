72913e0edd6b2c7b38b2bf0c65927288b2cd6751
ShenPaul <shenpaulheng@gmail.com>
2021-07-25 16:21:39 +0200

    adjust code

--- image_rec.py
+++ image_rec.py
@@ -11,7 +11,8 @@ from keras.utils import np_utils
 import numpy as np
 from matplotlib import pyplot as plt
 from keras.models import load_model
-#預測歷史
+
+#anticipation
 class LossHistory(keras.callbacks.Callback):
     def __init__(self,model,x_test, y_test):
         self.model = model
@@ -24,40 +25,38 @@ class LossHistory(keras.callbacks.Callback):
         for i in range(len(y_test)):
             if(y_test_pred[i]!=y_test[i]):
                 error+=1
-        print("\n104501019's Test Accurance: ",(len(y_test)*1.0-(error*1.0))/len(y_test))
+        print("\nTest Accurance: ",(len(y_test)*1.0-(error*1.0))/len(y_test))
         
-#讀取訓練資料路徑
+#set up path
 train_path = "./image"
 files = listdir(train_path)
 x_data=np.array([])
 data_number=0
-#讀取資料夾內所有檔案
+
+#Read all in the folder
 for root, dirs, files in walk(train_path):
     for f in files:
         if data_number==0:
             fullpath = join(root, f)
             im = Image.open(fullpath)
-            x_data = (np.array(im) / 255).reshape(1,28,28)  # 讀取資料時順便做資料正規化
-            #print(fullpath)
-            #print(x_data.shape)
+            x_data = (np.array(im) / 255).reshape(1,28,28)  
             data_number += 1
         else:
             fullpath = join(root, f)
-            im = Image.open(fullpath)
+            im = Image.open(fullpath) 
             im = (np.array(im)/255).reshape(1,28,28)
-            x_data = np.vstack((x_data,im)) # 讀取資料時順便做資料正規化
-            #print(fullpath)
-            #print(x_data.shape)
+            x_data = np.vstack((x_data,im)) 
             data_number += 1
-x_data=x_data.reshape(data_number,28,28,1) #調整資料格式
-#建立label
+x_data=x_data.reshape(data_number,28,28,1) #adjust data
+
+#build label
 y_data=[]
 for k in range(0,43,1):
     for i in range(0,10,1):
         for j in range(0,5,1):
             y_data.append(i)
 
-#讀取測試資料
+#read test data
 test_path = "./test_image"
 files = listdir(test_path)
 x_test=[]
@@ -68,12 +67,11 @@ for root, dirs, files in walk(test_path):
         im = Image.open(fullpath)
         p = np.array(im)/255
         x_test.append(p)
-        #print(fullpath)
         test_number+=1
 x_test=np.array(x_test)
 x_test=x_test.reshape(test_number,28,28,1)
 
-#建立test_label
+#build up test label
 y_test=[]
 for k in range(0,6,1):
     for i in range(0,10,1):
@@ -83,26 +81,30 @@ for k in range(0,6,1):
 #one hot encoding
 y_train = np_utils.to_categorical(y_data)
 
-#建立模型
+#buile up model
 model = Sequential()
 model.add(Conv2D(25, kernel_size=(3, 3),input_shape=(28, 28, 1)))
 model.add(MaxPool2D(pool_size=[2, 2]))
 model.add(Flatten())
 model.add(Dense(units=10, activation='softmax'))
-#設定model參數
+
+#set up the parameter of model
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-#設定訓練紀錄
+
+#setup record
 history = LossHistory(model, x_test, y_test)
-#開始訓練
+
+#start training
 train_history = model.fit(x_data, y_train,epochs=10,verbose=1,shuffle=True,callbacks=[history])
 
-#作圖
+#plot:to visualize the analysis
 plt.plot(train_history.history['loss'])   
 plt.title('Train History')  
 plt.ylabel('loss')  
 plt.xlabel('Epoch')  
 plt.legend(['loss'], loc='upper left')  
 plt.show() 
+
 #save model
 model.save('my_model.h5')
 del model
